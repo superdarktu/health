6c3492d11f10ede892a198b36451d3ba4e249ffb
superdarktu <superdarktu@gmail.com>
2017-05-19 17:30:10 +0800

    mmm

--- src/main/resources/static/js/teacher/plan.js
+++ src/main/resources/static/js/teacher/plan.js
@@ -1,19 +1,34 @@
 function foodadd(){
-	$(".foodadd").unbind();
-	$(".foodadd").click(function(){
-		$(this).parent().parent().before($("#food1").html());
-	});
+//	$("#foodadd2").unbind();
+    $("#foodadd2").click(function(){
+        temp = '<div class="food"><td rowspan="2">第'+($(".food").length+1)+'天</td>';
+        $(this).before(temp+$("#food2").html());
+        foodadd1();
+    });
 }
 function foodadd1(){
-	$(".foodadd1").unbind();
-	$(".foodadd1").click(function(){
-		temp = $(this).parent().parent().prev(".foodday").find(".day");
-		console.log($(this).parent().parent().prev(".foodday").html());
-		temp.attr("rowspan",parseInt(temp.attr("rowspan"))+1);
-		$(this).parent().parent().before($("#food2").html());
-	});
+    $(".foodadd1").unbind();
+    $(".foodadd1").click(function(){
+        $(this).parent().parent().before($("#food1").html());
+    });
+}
+function itemadd(){
+//	$("#foodadd2").unbind();
+    $("#itemadd2").click(function(){
+        temp = '<div class="item"><td rowspan="2">第'+($(".item").length+1)+'天</td>';
+        $(this).before(temp+$("#item2").html());
+        itemadd1();
+    });
+}
+function itemadd1(){
+    $(".itemadd1").unbind();
+    $(".itemadd1").click(function(){
+        $(this).parent().parent().before($("#item1").html());
+    });
 }
 $(document).ready(function () {
 	foodadd();
 	foodadd1();
+	itemadd();
+	itemadd1();
 })
